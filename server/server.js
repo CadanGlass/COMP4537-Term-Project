32f311db5f9c3b29200a0e3a86dc25741499e1fe
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-10-13 23:50:47 -0700

    test

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ function checkAdmin(req, res, next) {
 }
 
 // Register route (store user in SQLite DB with a hashed password and role)
-app.post("/register/app4", async (req, res) => {
+app.post("app4/register/", async (req, res) => {
   console.log("Register attempt with data:", req.body);
 
   const { username, password, role } = req.body;
