56838a8ef64cbf14c0fadf334bec604f9b9b2d45
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-11-04 16:11:31 -0800

    Revert "Update cors"
    This reverts commit cb9b2d8756f70906fe3aea60ce6a7998642d18fc.

--- server/server.js
+++ server/server.js
@@ -11,14 +11,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key"; // Use environme
 const SALT_ROUNDS = 10; // Number of salt rounds for bcrypt
 
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "https://comp4537-term-project.netlify.app", // Allow specific origin
-    methods: "GET,POST,PUT,DELETE,OPTIONS", // Define allowed methods
-    allowedHeaders: "Content-Type,Authorization", // Allow headers as needed
-  })
-);
+app.use(cors()); // Enable CORS for all routes
 
 // Global request logger middleware
 app.use((req, res, next) => {
