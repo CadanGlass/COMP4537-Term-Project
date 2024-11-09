7013048b2294dd7053e987524e52e619bb393d95
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-11-08 23:37:45 -0800

    Revert all changes to image-text generator page

--- client/src/components/ImageTextGenerator.jsx
+++ client/src/components/ImageTextGenerator.jsx
@@ -49,7 +49,6 @@ function ImageTextGenerator() {
     e.preventDefault();
 
     if (!selectedFile) {
-    if (!selectedFile) {
       setError("Please select an image file.");
       return;
     }
@@ -89,7 +88,6 @@ function ImageTextGenerator() {
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            "Content-Type": "multipart/form-data",
             "Content-Type": "multipart/form-data",
           },
           onUploadProgress: (progressEvent) => {
